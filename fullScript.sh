1e6f695831c97999fa4494dab9dec8a78c94b9a0
Andrew MacKinnon <amackinn@nd.edu>
2018-10-03 10:05:12 -0400

    combined scripts

---
+++ fullScript.sh
@@ -0,0 +1,22 @@
+echo "Highest Earner:"
+cat "$1" | cut -d ',' -f 1,2,4 | grep -v "gender" | sed 's/\,/\ /g'| sort -k 3n | tail -n 1
+echo "----------------------"
+echo "Lowest Earner:" 
+cat "$1" | cut -d ',' -f 1,2,4 | grep -v "gender" | sed 's/\,/\ /g'| sort -k 3n | head -n 1
+echo "----------------------"
+echo "Number of Women in Top 10:"
+cat "$1" | cut -d ',' -f 1,2,4 | grep -v "gender" | sed 's/\,/\ /g'| sort -k 3n | tail -n 10 | grep -c "female"
+echo "Highest Earner:"
+cat "$1" | cut -d ',' -f 1,2,4 | grep -v "gender" | sed 's/\,/\ /g'| sort -k 3n | tail -n 1
+echo "----------------------"
+echo "Lowest Earner:" 
+cat "$1" | cut -d ',' -f 1,2,4 | grep -v "gender" | sed 's/\,/\ /g'| sort -k 3n | head -n 1
+echo "----------------------"
+echo "Number of Women in Top 10:"
+cat "$1" | cut -d ',' -f 1,2,4 | grep -v "gender" | sed 's/\,/\ /g'| sort -k 3n | tail -n 10 | grep -c "female"
+echo "----------------------"
+echo "Wage difference with college education:"
+val1 = '$(cat "$1" | cut -d , -f 3,4 | grep 12, | sort -k3 -n | head -n 1 | cut -d , -f 2)'
+val2 = '$(cat "$1" | cut -d , -f 3,4 | grep 16, | sort -k3 -n | head -n 1 | cut -d , -f 2)'
+$val1 - $val2| bc
+
